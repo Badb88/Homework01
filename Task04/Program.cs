fa7f4efd071a1f035e3308ecb466e143495ce45f
OlegB <os.babkin@gmail.com>
2022-05-02 22:03:33 +0300

    Исправлено решение задачи 04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -8,9 +8,9 @@ int number = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= number; i++)
 {
     if (i % 2 == 0)
-    Console.Write(i);
-    if (i != number)
     {
+        Console.Write(i);
+    if (i != number)
         Console.Write(", ");
     }
 }
