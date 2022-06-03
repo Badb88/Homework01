ed9660c2e0084ff72083bae98b5b24de3e51b551
OlegB <os.babkin@gmail.com>
2022-06-03 18:15:41 +0300

    Добавил в решение else if

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -12,7 +12,7 @@ Console.Write("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 if (a > max) max = a;
-if (b > max) max = b;
+else if (b > max) max = b;
 if (c > max) max = c;
 Console.Write("Максимальное число: ");
 Console.WriteLine(max);
